f1e6912c9dd5a35f9bd00150594925f72aac6124
joshinakul <nakulj3010@outlook.com>
2020-05-08 04:51:11 +0530

    fixed issues

--- Src/model.py
+++ Src/model.py
@@ -6,10 +6,10 @@ class BERTModel(nn.Module):
     
     def __init__(self):
         
-        super(BERTModel,self).__init__():
-            self.bert = transformers.BertModel.from_pretrained(config.BERT_PATH)
-            self.bert_drop = nn.Dropout(0.2)
-            self.out = nn.Linear(768,1)
+        super(BERTModel,self).__init__()
+        self.bert = transformers.BertModel.from_pretrained(config.BERT_PATH)
+        self.bert_drop = nn.Dropout(0.2)
+        self.out = nn.Linear(768,1)
             
     def forward(self,ids,mask,token_type_ids):
         
